5d43ba82a887b4096c94c8c019a8611b8c4db4c9
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-07 02:47:44 +0600

    create_Categories created

--- controller/controller.js
+++ controller/controller.js
@@ -1,3 +1,19 @@
+const model = require("../models/model");
+
+function create_Categories(req, res) {
+  const Create = new model.Categories({
+    type: "Savings",
+    color: "#1F3B5C",
+  });
+
+  Create.save(function (err) {
+    if (!err) return res.json(Create);
+    return res
+      .status(400)
+      .json({ message: `Error while creating categories ${err}` });
+  });
+}
+
 function create_Categories(req, res) {
   res.json("route");
 }
--- routes/route.js
+++ routes/route.js
@@ -1,5 +1,5 @@
 const routes = require("express").Router();
 const controller = require("../controller/controller");
-routes.route("/api/categories").get(controller.create_Categories);
+routes.route("/api/categories").post(controller.create_Categories);
 
 module.exports = routes;

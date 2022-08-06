24aee83c787311e01c6a78f86183027d8a5fb940
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-07 01:05:18 +0600

    sever initialized

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-const express = require('express');
+const express = require("express");
+
+const app = express();
+
+const cors = require("cors");
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use(cors());
+
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
+
+app.get("/", (req, res) => {
+  res.send("hello from sever");
+});
